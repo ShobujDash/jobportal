0d7f98f3e89ff50421ba59f8a418ea878b10e54e
ShobujDas <shobujd6.gmail.com>
2024-07-30 23:39:05 +0600

    user and company model route controller complete

---
+++ backend/models/application.model.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+const applicationSchema = new mongoose.Schema(
+  {
+    job: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref:"Job"
+      required: true,
+    },
+    applicant: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref:"User"
+      required: true,
+    },
+    status: {
+      type: String,
+      emum:["pending","accepted","rejected"],
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Application = mongoose.model("Application", applicationSchema);
